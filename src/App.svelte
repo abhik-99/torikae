1acf35b7d2baacd340696736bc71497156b023ad
abhik-99 <abhik.banerjee.1999@gmail.com>
2021-10-27 22:33:43 +0530

    First Commit.

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<main>
+	<h1>Torikae</h1>
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		width: 100vw;
+		height: 100vh;
+		margin: 0 auto;
+		background-image: url("/assets/img/bg_with_circles.svg");
+
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
